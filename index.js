02fa684391181db9e6483527ac5939d168888a41
Aditya Anand <aditechdevofficial@gmail.com>
2023-08-02 16:01:32 +0530

    :fire: TODO user model

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const colors = require('colors');
 
+
+const user = require('./routes/user');
 
 const app = express();
+
+// Body Parser
+app.use(express.json());
 
 
 // load env vars
@@ -10,9 +16,23 @@ dotenv.config({
     path: './config/config.env'
 });
 
+const connectDb = require("./config/db")
+connectDb();
+app.use('/api/v1/users', user);
+
 const PORT = process.env.PORT || '3000';
 
 app.listen(PORT, console.log(
         `Server running in ${process.env.NODE_ENV} mode on PORT ${PORT}`
     ),
 );
+
+
+// Handle unhandle promise rejection
+process.on('unhandledRejection', (err, Promise) => {
+    console.log(`${err.message}`);
+    // close server and exit the process
+    server.close(
+        () => process.exit(1)
+    );
+});
